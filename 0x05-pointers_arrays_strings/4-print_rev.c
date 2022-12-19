5d85126f9d59f77f3a36528c649685d9e226d461
Clovey15 <oluredeem15@gmail.com>
2022-12-19 06:36:09 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - string to be printed in reverse followed by a new line
+ * @s: string in reverse
+ *
+ */
+
+void print_rev(char *s)
+{
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
+
