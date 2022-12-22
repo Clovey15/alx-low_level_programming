234856afd99b0cb11f6749a401eeaf43dc458490
Clovey15 <oluredeem15@gmail.com>
2022-12-21 19:40:16 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ *_strcat - function to concatenates two strings
+ * @src: append from
+ * @dest: append to
+ * Return: char and a string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+	return (dest);
+}
